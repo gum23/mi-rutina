922c20e1181fd64627f7d6a102ba130c19e70730
Eduardo Unrrein <gum23coder@gmail.com>
2024-01-05 16:24:05 -0300

    Se agregaron S-R-C de clients y personal

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import { app, PORT } from "./app.js";
-import cors from "cors";
 import "./db/connect.db.js"
 
-//Uso de cors
-app.use(cors());
-
 //route
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello world!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listen in port: ${PORT}`);
